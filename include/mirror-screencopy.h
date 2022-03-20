7a78782f311d3d4c27526bfe52610ecd2a184396
Ferdinand Bachmann <theferdi265@gmail.com>
2022-03-20 20:09:16 +0100

    src/mirror-screencopy: implement basic screencopy boilerplate (incomplete)
    - blocked on knowing how to deal with format codes and getting it into
      an OpenGL texture

--- include/mirror-screencopy.h
+++ include/mirror-screencopy.h
@@ -7,11 +7,20 @@
 #include <wayland-client.h>
 
 typedef enum {
-    STATE_WAIT_FRAME,
+    STATE_WAIT_BUFFER,
+    STATE_WAIT_BUFFER_DONE,
+    STATE_WAIT_FLAGS,
     STATE_WAIT_READY,
     STATE_READY,
     STATE_CANCELED
 } screencopy_state_t;
+
+typedef struct {
+    uint32_t width;
+    uint32_t height;
+    uint32_t stride;
+    uint32_t format;
+} buffer_info_t;
 
 typedef struct {
     mirror_backend_t header;
@@ -19,13 +28,19 @@ typedef struct {
     // shm state
     int shm_fd;
     size_t shm_size;
+    void * shm_addr;
 
     // wl_shm objects
     struct wl_shm_pool * shm_pool;
     struct wl_buffer * shm_buffer;
 
-    // screencopy objects
+    // screencopy frame object
     struct zwlr_screencopy_frame_v1 * screencopy_frame;
+
+    // frame data
+    buffer_info_t buffer_info;
+    buffer_info_t frame_info;
+    uint32_t frame_flags;
 
     // screencopy state flags
     screencopy_state_t state;
--- src/mirror-screencopy.c
+++ src/mirror-screencopy.c
@@ -14,6 +14,37 @@ static void screencopy_frame_event_buffer(
 ) {
     ctx_t * ctx = (ctx_t *)data;
     screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
+
+    log_debug(ctx, "screencopy_frame: received buffer offer for %dx%d+%d frame\n", width, height, stride);
+    if (backend->state != STATE_WAIT_BUFFER) {
+        log_error("screencopy_frame: got buffer event while in state %d\n", backend->state);
+        backend_fail(ctx);
+        return;
+    }
+
+    backend->frame_info.width = width;
+    backend->frame_info.height = height;
+    backend->frame_info.stride = stride;
+    backend->frame_info.format = format;
+
+    backend->state = STATE_WAIT_BUFFER_DONE;
+
+    (void)frame;
+}
+
+static void screencopy_frame_event_linux_dmabuf(
+    void * data, struct zwlr_screencopy_frame_v1 * frame,
+    uint32_t format, uint32_t width, uint32_t height
+) {
+    ctx_t * ctx = (ctx_t *)data;
+    screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
+
+    log_debug(ctx, "screencopy_frame: received dmabuf offer for %dx%d frame\n", width, height);
+    log_debug(ctx, "screencopy_frame: dmabuf buffers are not yet supported\n");
+
+    (void)backend;
+    (void)frame;
+    (void)format;
 }
 
 static void screencopy_frame_event_buffer_done(
@@ -21,17 +52,68 @@ static void screencopy_frame_event_buffer_done(
 ) {
     ctx_t * ctx = (ctx_t *)data;
     screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
-}
 
-static void screencopy_frame_event_linux_dmabuf(
-    void * data, struct zwlr_screencopy_frame_v1 * frame,
-    uint32_t format, uint32_t width, uint32_t height
-) {
-    (void)data;
+    log_debug(ctx, "screencopy_frame: received buffer done event\n");
+    if (backend->state != STATE_WAIT_BUFFER_DONE) {
+        log_error("screencopy_frame: received buffer_done without supported buffer offer\n");
+        backend_fail(ctx);
+        return;
+    }
+
+    size_t new_size = backend->frame_info.stride * backend->frame_info.height;
+    if (new_size > backend->shm_size) {
+        if (backend->shm_buffer != NULL) {
+            wl_buffer_destroy(backend->shm_buffer);
+            backend->shm_buffer = NULL;
+        }
+
+        if (ftruncate(backend->shm_fd, new_size) == -1) {
+            log_error("screencopy_frame: failed to grow shm buffer\n");
+            backend_fail(ctx);
+            return;
+        }
+
+        void * new_addr = mremap(backend->shm_addr, backend->shm_size, new_size, MREMAP_MAYMOVE);
+        if (new_addr == MAP_FAILED) {
+            log_error("screencopy_frame: failed to remap shm buffer\n");
+            backend_fail(ctx);
+            return;
+        }
+
+        backend->shm_addr = new_addr;
+        backend->shm_size = new_size;
+
+        wl_shm_pool_resize(backend->shm_pool, new_size);
+    }
+
+    bool new_buffer_needed =
+        backend->buffer_info.width != backend->frame_info.width ||
+        backend->buffer_info.height != backend->frame_info.height ||
+        backend->buffer_info.stride != backend->frame_info.stride ||
+        backend->buffer_info.format != backend->frame_info.format;
+
+    if (backend->shm_buffer != NULL && new_buffer_needed) {
+        wl_buffer_destroy(backend->shm_buffer);
+        backend->shm_buffer = NULL;
+    }
+
+    if (backend->shm_buffer == NULL) {
+        backend->shm_buffer = wl_shm_pool_create_buffer(
+            backend->shm_pool, 0,
+            backend->frame_info.width, backend->frame_info.height,
+            backend->frame_info.stride, backend->frame_info.format
+        );
+        if (backend->shm_buffer == NULL) {
+            log_error("screencopy_frame: failed to create wl_buffer\n");
+            backend_fail(ctx);
+            return;
+        }
+    }
+
+    backend->state = STATE_WAIT_FLAGS;
+    zwlr_screencopy_frame_v1_copy(backend->screencopy_frame, backend->shm_buffer);
+
     (void)frame;
-    (void)format;
-    (void)width;
-    (void)height;
 }
 
 static void screencopy_frame_event_damage(
@@ -52,6 +134,18 @@ static void screencopy_frame_event_flags(
 ) {
     ctx_t * ctx = (ctx_t *)data;
     screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
+
+    log_debug(ctx, "screencopy_frame: received flags event\n");
+    if (backend->state != STATE_WAIT_FLAGS) {
+        log_error("screencopy_frame: received unexpected flags event\n");
+        backend_fail(ctx);
+        return;
+    }
+
+    backend->frame_flags = flags;
+    backend->state = STATE_WAIT_READY;
+
+    (void)frame;
 }
 
 static void screencopy_frame_event_ready(
@@ -60,6 +154,25 @@ static void screencopy_frame_event_ready(
 ) {
     ctx_t * ctx = (ctx_t *)data;
     screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
+
+    log_debug(ctx, "screencopy_frame: received ready event\n");
+    log_debug(ctx, "width: %d, height: %d, stride: %d, format: %x\n",
+        backend->frame_info.width, backend->frame_info.height,
+        backend->frame_info.stride, backend->frame_info.format
+    );
+
+    backend_fail(ctx);
+
+    // TODO: somehow create OpenGL texture
+    // (lots of cleanup needed elsewhere too)
+    // (EGLImage might be able to be removed from common mirror and moved into dmabuf)
+
+    zwlr_screencopy_frame_v1_destroy(backend->screencopy_frame);
+    backend->screencopy_frame = NULL;
+    backend->state = STATE_READY;
+    backend->header.fail_count = 0;
+
+    (void)frame;
 }
 
 static void screencopy_frame_event_failed(
@@ -67,12 +180,19 @@ static void screencopy_frame_event_failed(
 ) {
     ctx_t * ctx = (ctx_t *)data;
     screencopy_mirror_backend_t * backend = (screencopy_mirror_backend_t *)ctx->mirror.backend;
+
+    backend->header.fail_count++;
+    zwlr_screencopy_frame_v1_destroy(backend->screencopy_frame);
+    backend->screencopy_frame = NULL;
+    backend->state = STATE_CANCELED;
+
+    (void)frame;
 }
 
 static const struct zwlr_screencopy_frame_v1_listener screencopy_frame_listener = {
     .buffer = screencopy_frame_event_buffer,
-    .buffer_done = screencopy_frame_event_buffer_done,
     .linux_dmabuf = screencopy_frame_event_linux_dmabuf,
+    .buffer_done = screencopy_frame_event_buffer_done,
     .damage = screencopy_frame_event_damage,
     .flags = screencopy_frame_event_flags,
     .ready = screencopy_frame_event_ready,
@@ -86,7 +206,16 @@ static void mirror_screencopy_on_frame(ctx_t * ctx) {
 
     if (backend->state == STATE_READY || backend->state == STATE_CANCELED) {
         log_debug(ctx, "mirror_screencopy_on_frame: clearing screencopy_frame state\n");
-        backend->state = STATE_WAIT_FRAME;
+
+        buffer_info_t zero_buffer = {
+            .width = 0,
+            .height = 0,
+            .stride = 0,
+            .format = 0
+        };
+        backend->frame_info = zero_buffer;
+        backend->frame_flags = 0;
+        backend->state = STATE_WAIT_BUFFER;
 
         backend->screencopy_frame = zwlr_screencopy_manager_v1_capture_output(
             ctx->wl.screencopy_manager, ctx->opt.show_cursor, ctx->mirror.current_target->output
@@ -109,6 +238,7 @@ static void mirror_screencopy_on_cleanup(ctx_t * ctx) {
     if (backend->screencopy_frame != NULL) zwlr_screencopy_frame_v1_destroy(backend->screencopy_frame);
     if (backend->shm_buffer != NULL) wl_buffer_destroy(backend->shm_buffer);
     if (backend->shm_pool != NULL) wl_shm_pool_destroy(backend->shm_pool);
+    if (backend->shm_addr != NULL) munmap(backend->shm_addr, backend->shm_size);
     if (backend->shm_fd != -1) close(backend->shm_fd);
 
     free(backend);
@@ -138,9 +268,21 @@ void init_mirror_screencopy(ctx_t * ctx) {
 
     backend->shm_fd = -1;
     backend->shm_size = 0;
+    backend->shm_addr = NULL;
     backend->shm_pool = NULL;
     backend->shm_buffer = NULL;
+
     backend->screencopy_frame = NULL;
+
+    buffer_info_t zero_buffer = {
+        .width = 0,
+        .height = 0,
+        .stride = 0,
+        .format = 0
+    };
+    backend->buffer_info = zero_buffer;
+    backend->frame_info = zero_buffer;
+    backend->frame_flags = 0;
 
     backend->state = STATE_READY;
 
@@ -155,6 +297,13 @@ void init_mirror_screencopy(ctx_t * ctx) {
     backend->shm_size = 1;
     if (ftruncate(backend->shm_fd, backend->shm_size) == -1) {
         log_error("init_mirror_screencopy: failed to resize shm buffer\n");
+        backend_fail(ctx);
+    }
+
+    backend->shm_addr = mmap(NULL, backend->shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, backend->shm_fd, 0);
+    if (backend->shm_addr == MAP_FAILED) {
+        backend->shm_addr = NULL;
+        log_error("init_mirror_screencopy: failed to map shm buffer\n");
         backend_fail(ctx);
     }
 
