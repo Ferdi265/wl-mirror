19b854c0923d47bfb1d343e36002803f4c444d2f
Ferdinand Bachmann <theferdi265@gmail.com>
2023-01-03 18:29:21 +0100

    src/mirror-xdg-portal: make missing xdg_exporter non-fatal

--- src/mirror-xdg-portal.c
+++ src/mirror-xdg-portal.c
@@ -918,9 +918,7 @@ static void on_loop_pw_event(ctx_t * ctx) {
 void init_mirror_xdg_portal(ctx_t * ctx) {
     // check for required protocols
     if (ctx->wl.xdg_exporter == NULL) {
-        // TODO: this should not be fatal
-        log_error("mirror-xdg-portal::init(): missing xdg_foreign protocol\n");
-        return;
+        log_warn("mirror-xdg-portal::init(): missing xdg_foreign protocol\n");
     }
 
     // allocate backend context structure
