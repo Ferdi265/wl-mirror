83833e23eb4be504ec1937de6f10d4c2a381ac58
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-01-17 21:58:56 +0100

    glsl: temporarily replace texture with colorful pattern

--- glsl/fragment_shader.glsl
+++ glsl/fragment_shader.glsl
@@ -6,7 +6,8 @@ uniform bool uInvertColors;
 varying vec2 vTexCoord;
 
 void main() {
-    vec4 color = texture2D(uTexture, vTexCoord);
+    //vec4 color = texture2D(uTexture, vTexCoord);
+    vec4 color = vec4(vTexCoord.x, vTexCoord.y, 1.0 - (vTexCoord.x + vTexCoord.y) / 2.0, 1.0);
     if (uInvertColors) {
         gl_FragColor = vec4(1.0 - color.r, 1.0 - color.g, 1.0 - color.b, 1.0);
     } else {
