af76781fd362b553eced4a8a9c4a27c0cd3e53cf
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-08-06 23:17:05 +0200

    src/mirror-xdg-portal: don't require xdg_foreign

--- src/mirror-xdg-portal.c
+++ src/mirror-xdg-portal.c
@@ -1315,11 +1315,6 @@ static void on_loop_pw_event(ctx_t * ctx) {
 // --- init_mirror_xdg_portal ---
 
 void wlm_mirror_xdg_portal_init(ctx_t * ctx) {
-    // check for required protocols
-    if (ctx->wl.xdg_exporter == NULL) {
-        wlm_log_warn("mirror-xdg-portal::init(): missing xdg_foreign protocol\n");
-    }
-
     // allocate backend context structure
     xdg_portal_mirror_backend_t * backend = malloc(sizeof (xdg_portal_mirror_backend_t));
     if (backend == NULL) {
