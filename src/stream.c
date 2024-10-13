100718b584bcd3bc2c9a822db6e39d79abc45763
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-10-13 13:32:54 +0200

    src/stream: correctly handle EPOLLIN and EPOLLHUP
    This fixes wl-mirror staying open when its input closes in stream mode.
    Previously wl-mirror also hanged in this case as it attempted to read
    stdin in response to the EPOLLHUP event.
    
    Partial fix for #51.

--- src/stream.c
+++ src/stream.c
@@ -118,7 +118,11 @@ static void on_line(ctx_t * ctx, char * line) {
 }
 
 static void on_stream_data(ctx_t * ctx, uint32_t events) {
-    (void)events;
+    // close the window if the input option stream closed
+    if ((events & EPOLLHUP) != 0) wlm_wayland_window_close(ctx);
+
+    // return early if there is nothing to read
+    if ((events & EPOLLIN) == 0) return;
 
     while (true) {
         line_reserve(ctx);
